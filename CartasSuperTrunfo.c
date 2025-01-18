6ff2b0f3a5bfd128837bbbfbbd9b5260eeca7584
Anderson36310 <111932714+Anderson36310@users.noreply.github.com>
2025-01-18 16:24:42 +0000

    adicionei commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,6 +8,7 @@
 
 int main() {
     printf("Desafios de cartas\n");
+    printf("comit \n");
 
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
