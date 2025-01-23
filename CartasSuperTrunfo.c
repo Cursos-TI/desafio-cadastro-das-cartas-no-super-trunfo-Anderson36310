bb3ed5b4f19fceee7cd1763df7ad242cd30a471a
Anderson36310 <111932714+Anderson36310@users.noreply.github.com>
2025-01-23 14:32:09 +0000

    Nivel básico concluido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,35 +13,46 @@
 
 int main(){
    
-    char estado;
-    char nome_cidade[20];
-    char cod_carta[20];
+    char estado[10];
+    char cidade[10];
+    char cod_carta[10];
     int populacao;
     int pontos_turisticos;
     float area;
     float pib;
 
     printf("Olá! Seja Bem vindo ao jogo de cartas Super Trunfo! Vamos começar...\n");
+    
     printf("Digite o Estado: ");
-    scanf("%c\n", &estado);
-
+    scanf("%s", estado);
+    
     printf("Digite o Nome da Cidade: ");
-    scanf("%s\n", nome_cidade);
+    scanf("%s", cidade);
 
     printf("Digite o Código da carta: ");
-    scanf("%s\n", cod_carta);
+    scanf("%s", cod_carta);
 
     printf("Digite o número da população: ");
-    scanf("%d\n", &populacao);
+    scanf("%d", &populacao);
 
     printf("Digite a quantidade de pontos Turísticos: ");
-    scanf("%d\n", &pontos_turisticos);
+    scanf("%d", &pontos_turisticos);
 
     printf("Digite o tamanho da área em KM: ");
-    scanf("%f\n", &area);
+    scanf("%f", &area);
 
     printf("Digite o Pib da Cidade: ");
-    scanf("%f\n", &pib);
+    scanf("%f", &pib);
 
+    // Dados a serem mostrados para o usuário
+    printf("Estes são os dados da primeira carta \n");
+    printf("Estado:%s\n", estado);
+    printf("Cidade:%s\n", cidade);
+    printf("Cód.Carta:%s\n", cod_carta);
+    printf("População:%d\n", populacao);
+    printf("Pontos Turisticos:%d\n", pontos_turisticos);
+    printf("Aréa em KM:%f\n");
+
+    return 0;
 
 }
